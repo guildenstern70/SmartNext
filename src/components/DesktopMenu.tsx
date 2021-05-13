7911d20cdc3b25345e87ef96892d14c3ae8e031a
Alessio Saltarin <alessiosaltarin@gmail.com>
2021-05-13 16:36:55 +0200

    Added Registration Form

--- src/components/DesktopMenu.tsx
+++ src/components/DesktopMenu.tsx
@@ -9,33 +9,53 @@ import { MenuProps } from '../types/MenuProps';
 import { Icon, Menu } from 'semantic-ui-react';
 import Link from 'next/link';
 
-const DesktopMenu: React.FC<MenuProps> = (props: MenuProps) => (
-    <Menu>
-        <Link href="/" passHref>
-            <Menu.Item as="a" name="home" active={props.activeItem === 'home'}>
-                <Icon name="home" />
-                Home
-            </Menu.Item>
-        </Link>
-        <Link href="/games" passHref>
-            <Menu.Item as="a" name="games" active={props.activeItem === 'games'}>
-                <Icon name="gamepad" />
-                Games
-            </Menu.Item>
-        </Link>
-        <Link href="/channels" passHref>
-            <Menu.Item as="a" name="channels" active={props.activeItem === 'channels'}>
-                <Icon name="camera" />
-                Channels
-            </Menu.Item>
-        </Link>
-        <Link href="/info" passHref>
-            <Menu.Item as="a" name="info" active={props.activeItem === 'info'}>
-                <Icon name="play" />
-                Info
-            </Menu.Item>
-        </Link>
-    </Menu>
-);
+class DesktopMenu extends React.Component<MenuProps> {
+    constructor(props: MenuProps) {
+        super(props);
+    }
+
+    signup() {}
+
+    render(): React.ReactNode {
+        return (
+            <Menu>
+                <Link href="/" passHref>
+                    <Menu.Item as="a" name="home" active={this.props.activeItem === 'home'}>
+                        <Icon name="home" />
+                        Home
+                    </Menu.Item>
+                </Link>
+                <Link href="/games" passHref>
+                    <Menu.Item as="a" name="games" active={this.props.activeItem === 'games'}>
+                        <Icon name="gamepad" />
+                        Games
+                    </Menu.Item>
+                </Link>
+                <Link href="/channels" passHref>
+                    <Menu.Item as="a" name="channels" active={this.props.activeItem === 'channels'}>
+                        <Icon name="camera" />
+                        Channels
+                    </Menu.Item>
+                </Link>
+                <Link href="/info" passHref>
+                    <Menu.Item as="a" name="info" active={this.props.activeItem === 'info'}>
+                        <Icon name="play" />
+                        Info
+                    </Menu.Item>
+                </Link>
+                <Link href="/login" passHref>
+                    <Menu.Menu position="right">
+                        <Menu.Item
+                            as="a"
+                            name="login"
+                            active={this.props.activeItem === 'login'}
+                            onClick={this.signup}
+                        />
+                    </Menu.Menu>
+                </Link>
+            </Menu>
+        );
+    }
+}
 
 export default DesktopMenu;
