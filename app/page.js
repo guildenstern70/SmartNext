cae1c4c2e24edbc4b3cc098d214948230dc5c3ec
Alessio Saltarin <alessiosaltarin@gmail.com>
2023-11-15 15:22:45 +0100

    New colors

--- app/page.js
+++ app/page.js
@@ -12,18 +12,19 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className={styles.main}>
-        <h1>SmartNext</h1>
-        <i>A Next.js template to jump start</i>
+        <h1 className="has-text-primary">SmartNext</h1>
+        <i className="mt-6">A</i>
         <Image
             src="/next.svg"
-            width={200}
-            height={200}
+            width={160}
+            height={160}
             alt="Next.js Logo" />
+        <i className="mb-6">template to start with</i>
         <div className="buttons">
             <Link href="/home" className="button is-primary">Enter</Link>
             <a href={'https://github.com/guildenstern70/SmartNext'}
                target={'_blank'}
-               className="button is-link">Boilerplate</a>
+               className="button is-link">Codebase</a>
         </div>
     </main>
   )
