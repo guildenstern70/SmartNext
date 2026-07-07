4fac761e01c6426101f9f25e09ace35bc19b5dfa
Alessio Saltarin <alessiosaltarin@gmail.com>
2026-07-07 11:53:46 +0200

    Upgraded to Next.js v.16

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const path = require('path')
 
-const nextConfig = {}
-
-module.exports = {
+const nextConfig = {
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
+        silenceDeprecations: ['legacy-js-api', 'import'],
     },
 }
+
+module.exports = nextConfig
